29821d977789eb5b7febf7ffc9d4ddb8a78ae6f4
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-05-26 12:24:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from flask import Flask,render_template,request,session
-import random
-
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 app.secret_key = '1234'
-
-
 
 @app.route('/',methods=['GET',"POST"])
 def roman():
